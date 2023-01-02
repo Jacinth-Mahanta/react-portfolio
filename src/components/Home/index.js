4c5d7f3baf1edd36dc853c57a443e0dccc884bc0
Jacinth Mahanta <mahanta.jacinth2021@gmail.com>
2023-01-02 23:14:10 +0530

    Letter Animation started

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,28 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
-
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['a','c','i','n','t','h']
+    const jobArray = ['W','e','b',' ','D','e','v','e','l','o','p','e','r', '.']
 
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-                    Hi, <br/> I'm
-                    <img src={LogoTitle} alt="developer" />
-                    acinth
+                    <span className={letterClass}>H</span>
+                    <span className={'${letterClass} _12'}>i,</span>
                     <br/>
-                    Web Developer
+                    <span className={'${letterClass} _13'}>I</span>
+                    <span className={'${letterClass} _14'}>'m</span>
+
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+                    <br/>
+                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22}/>
                 </h1>
 
                 <h2>
